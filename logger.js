6ac635c70161a7faa7c8257935ee1e754d750d13
Space Deck <thespacedeck@gmail.com>
2021-01-27 21:02:23 +0200

    added bunyan serializers

--- logger.js
+++ logger.js
@@ -22,6 +22,13 @@ module.exports = class Logger {
     /**
      * Register this logger for use with Google Stackdriver.
      * Undefined values may be replaced with defaults
+     * 
+     * @param fatal: The service/app is going to stop or become unusable now. An operator should definitely look into this soon.
+     * @param error: Fatal for a particular request, but the service/app continues servicing other requests. An operator should look at this soon(ish). Include as much information as possible to assist in a postmortem analysis of the error, be mindful with sensitive data.
+     * @param warn: A note on something that should probably be looked at by an operator eventually. Data inconsistencies, responses from 3rd party services that do not comply with their API specifications.
+     * @param info: Detail on regular operation.
+     * @param debug: Anything else, i.e. too verbose to be included in “info” level.
+     * @param trace: Logging from external libraries used by your app or very detailed application logging.
      */
     createLogger(constructorOptions) {
         // Creates a Bunyan Stackdriver Logging client
@@ -35,17 +42,18 @@ module.exports = class Logger {
             loggingBunyan.stream(constructorOptions.level ? constructorOptions.level : 'info'),
         ]
 
-        // if(process.env.NODE_ENV !== "production"){
-        //     streamsObj.push(
-        //         // Log to the console at 'info' and above
-        //         {stream: process.stdout, level: constructorOptions.level ? constructorOptions.level : 'info'}
-        //     )
-        // }
+        if(process.env.NODE_ENV !== "production"){
+            streamsObj.push(
+                // Log to the console at 'info' and above
+                {stream: process.stdout, level: constructorOptions.level ? constructorOptions.level : 'info'}
+            )
+        }
 
         let constructor = {
             ...{ 
                 name: this.serviceName,
-                streams: streamsObj
+                streams: streamsObj,
+                serializers: bunyan.stdSerializers
             }, 
             ...constructorOptions
         }
